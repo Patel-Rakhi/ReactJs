82295dd3d15c3cd9eb636d8f98d00a09a02d4cf1
RakhiPatel <rakhip817@gmail.com>
2024-12-04 21:52:32 +0530

    testing git

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const heading = React.createElement("h1",
-    {},
-    "Hello World!! From React!"
+const heading = React.createElement("div",
+    { id: "parent" },
+    [React.createElement('h1', { id: "abc" }, "Hello from nested Elements"),
+    React.createElement('h2', { id: "csd" }, "Hello from nested Elements")
+    ]
 )
-const root=ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(heading);
